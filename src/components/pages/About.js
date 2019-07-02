8758d7d8296cf489142f1cb6d1f77f468881ba58
Teemo <motchleuterio@gmail.com>
2019-07-02 22:38:31 +0800

    about page edit

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default props => {
   return (
     <div>
-      <h1 className="display-4">About Players</h1>
+      <h1 className="display-4 primaryText2">About Players</h1>
+      <p>This app is created for learning and testing React basics</p>
     </div>
   )
 }
